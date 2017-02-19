2770292541f3755d1e41b1896a86f4ea25d90815
Alexander Szostek <aszostek@udel.edu>
2017-02-19 11:24:12 -0500

    Initial commit

---
+++ app/src/main/java/udel/aszostek/eaglemunc/Main.java
@@ -0,0 +1,31 @@
+package udel.aszostek.eaglemunc;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int tabId)
+            {
+                //change to appropriate fragment
+                switch(tabId)
+                {
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+}
